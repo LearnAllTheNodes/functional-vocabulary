9c75a9fbb384e4c4d37801dbc66387a970c5fb47
Ethan Garofolo <ethan@suchsoftware.com>
2016-01-03 07:17:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Learn All The Nodes Episode 49
+
+This is the code behind Learn All The Nodes Episode 49, which demonstrates the function `map`.
+
+[Watch Episode 49](https://www.learnallthenodes.com/episodes/49-functional-vocabulary-map)
---
+++ aBetterWay.js
@@ -0,0 +1,15 @@
+var greatOldOnes = [
+  { id: 1, name: 'Cthulhu', rating: 5, description: 'A mix between a giant human, an octopus, and a dragon. He is usually depicted as being over hundreds of meters tall, with webbed human-looking arms, legs, and a pair of rudimentary wings on his back.' }
+, { id: 2, rating: 2, name: 'Byatis', description: 'Appears as a gigantic multicolored toad with one eye, a proboscis, crab-like claws, and tentacles below the mouth.' }
+, { id: 3, name: 'Dythalla', rating: 1, description: 'A gigantic saurian creature similar to Bokrug, but terrestrial, and endowed with a mane of tentacles.' }
+, { id: 4, name: 'Zalgo', rating: 5, description: 'Released when you are not consistent with asynchronous APIs.  Also Zalgo is a meme where a person takes a popular picture/comic and corrupts it, with scary results.' }
+, { id: 5, name: 'PHP', rating: 0, description: 'PHP is a server-side scripting language designed for web development but also used as a general-purpose programming language.' }      
+]
+
+function pluckName(item) {
+  return item.name
+}
+    
+var names = greatOldOnes.map(pluckName)
+
+console.log(names)
